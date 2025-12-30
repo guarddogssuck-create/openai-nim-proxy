b74f4153f58193d73bb167e255fef85b352d21cf
guarddogssuck-create <guarddogssuck@gmail.com>
2025-12-31 03:04:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const ENABLE_THINKING_MODE = true; // Set to true to enable chat_template_kwargs
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'deepseek-ai/deepseek-r1-0528',
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
-  'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
+  'gpt-4-turbo': 'moonshotai/kimi-k2-thinking',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1-terminus',
   'claude-3-opus': 'openai/gpt-oss-120b',
   'claude-3-sonnet': 'deepseek-ai/deepseek-v3.2',
